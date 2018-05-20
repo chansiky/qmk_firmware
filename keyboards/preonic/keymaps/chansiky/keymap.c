999ab460ee922c4ed2a4cdda00fbd312b5700ea7
chansiky <chansiky@gmail.com>
2018-05-20 11:32:08 -0500

    changes to ctrl and enter

--- keyboards/preonic/keymaps/chansiky/keymap.c
+++ keyboards/preonic/keymaps/chansiky/keymap.c
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_PIPE},
   {KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT)},
-  {KC_ENT,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC}
+  {KC_LCTL, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC}
 },
 
 
@@ -151,8 +151,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = {
-  {_______,    _______, _______, _______, _______,  _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS},
-  {TG(_RAISE), KC_PGUP, KC_HOME, KC_UP,   KC_END,   KC_DEL,  KC_BSPC, _______, _______, _______, KC_LBRC, KC_RBRC},
+  {_______,    _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, KC_DEL },
+  {TG(_RAISE), KC_PGUP, KC_HOME, KC_UP,   KC_END,   KC_DEL,  KC_BSPC, _______, _______, _______, _______, _______},
   {_______,    KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT,  _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ENT,  KC_ENT },
   {_______,    _______, _______, _______, _______,  KC_ENT,  KC_ENT,  _______, _______, _______, _______, _______},
   {_______,    _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______}
@@ -178,7 +178,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {_______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
 }
-
 
 };
 
