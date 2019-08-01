f95746d0aa33afe91a9369a5e361831c43a33e76
Chan Youn <chansiky@gmail.com>
2019-08-01 09:15:57 -0500

    update

--- keyboards/lets_split/keymaps/chansiky/keymap.c
+++ keyboards/lets_split/keymaps/chansiky/keymap.c
@@ -26,29 +26,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_ortho_4x12( \
    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT, \
-   KC_GRV,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RGUI, KC_RALT, KC_RCTL, KC_BSLS \
+   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+   KC_GRV,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RGUI, KC_RALT, KC_RCTL, KC_ENT \
 ),
 
 [_COLEMAK] = LAYOUT_ortho_4x12( \
-  KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC, \
-  KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-  ADJUST,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
 ),
 
 [_DVORAK] = LAYOUT_ortho_4x12( \
-  KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSPC, \
-  KC_ESC,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_SLSH, \
-  KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_ENT , \
-  ADJUST,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
 ),
 
 [_LOWER] = LAYOUT_ortho_4x12( \
  _______,  KC_F9, KC_F10, KC_F11,     KC_F12,_______,     _______,   KC_7,   KC_8,   KC_9,KC_SLSH,KC_PAST, \
  _______,  KC_F5,  KC_F6,  KC_F7,      KC_F8,_______,     _______,   KC_4,   KC_5,   KC_6,KC_MINS, KC_EQL, \
  _______,  KC_F1,  KC_F2,  KC_F3,      KC_F4,_______,     _______,   KC_1,   KC_2,   KC_3,KC_UNDS,KC_PPLS, \
- _______, _______, _______, _______, _______, _______,      _______,   KC_0, _______, _______, _______, _______\
+ _______, _______, _______, _______, _______,_______,     _______,   KC_0,_______,_______,_______,_______\
 ),
 
 
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,KC_TILD,KC_HOME,  KC_UP,  KC_END,KC_PGUP,     _______, KC_INS,KC_LPRN,KC_RPRN,KC_PIPE, _______, \
   KC_CAPS, KC_GRV,KC_LEFT,KC_DOWN,KC_RIGHT,KC_PGDN,     _______, KC_DEL,KC_LBRC,KC_RBRC,KC_BSLS, KC_ENT, \
   _______,KC_BRID,KC_BRIU,KC_VOLD, KC_VOLU,KC_MUTE,     _______,KC_BSPC,KC_LCBR,KC_RCBR,KC_SLSH, _______, \
-  _______, _______, _______, _______,  _______,_______,     _______, _______,KC_LABK,KC_RABK, _______, _______ \
+  _______, _______, _______, _______,  _______,_______, _______, _______,KC_LABK,KC_RABK, _______, _______ \
 ),
 
 
