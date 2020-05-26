59dc07d1e4fff6483e118d63f322138e8d8371d4
Chan Youn <chansiky@gmail.com>
2020-05-26 09:33:17 -0500

    config: leftpad: keymap update

--- keyboards/kyria/keymaps/leftpad/keymap.c
+++ keyboards/kyria/keymaps/leftpad/keymap.c
@@ -28,28 +28,28 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
-      KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                     KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC,
-      KC_ESC,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                     KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-      KC_LSFT,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,  RAISE, KC_LALT, KC_RALT,  LOWER,   KC_N,   KC_M, KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),
-                              RAISE ,KC_LGUI, LOWER,  KC_SPC, KC_LCTL, KC_RCTL, KC_SPC, RAISE, KC_RGUI, LOWER
+            KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                        KC_Y,     KC_U,   KC_I,    KC_O,     KC_P, KC_BSPC,
+            KC_ESC,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                        KC_H,     KC_J,   KC_K,    KC_L,  KC_SCLN, KC_QUOT,
+            KC_LSFT,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,  RAISE, KC_LALT, KC_RALT,   LOWER,     KC_N,     KC_M,KC_COMM,  KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),
+                                    RAISE ,KC_LGUI,  LOWER,  KC_SPC, KC_LCTL, KC_RCTL,  KC_SPC,   RAISE,   KC_RGUI, LOWER
     ),
     [_LOWER] = LAYOUT(
-       KC_GRV, KC_COMM,  KC_7,   KC_8,   KC_9,  KC_DOT,                                     KC_HOME, KC_PGDN, KC_PGUP,  KC_END, _______, _______,
-      KC_MINS, KC_0,     KC_4,   KC_5,   KC_6, KC_PPLS,                                     KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, _______, _______,
-      _______, KC_UNDS,  KC_1,   KC_2,   KC_3,  KC_EQL, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, _______,
-                                _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+            KC_GRV, KC_COMM,  KC_7,   KC_8,   KC_9, KC_BSPC,                                     KC_HOME, KC_PGDN, KC_PGUP,  KC_END, _______, _______,
+           KC_MINS, KC_0,     KC_4,   KC_5,   KC_6, KC_PPLS,                                     KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, _______, KC_MINS,
+     LSFT_T(KC_ENT),KC_UNDS,  KC_1,   KC_2,   KC_3,  KC_EQL, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, KC_UNDS, _______,
+                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
     [_RAISE] = LAYOUT(
-      KC_DEL,  KC_LPRN, KC_SLSH,   KC_UP, KC_BSLS, KC_RPRN,                                     _______,  KC_INS, KC_LPRN, KC_RPRN, KC_PIPE,  KC_DEL,
-     KC_CAPS,  KC_LBRC, KC_LEFT, KC_DOWN,KC_RIGHT, KC_RBRC,                                     _______, _______, KC_LBRC, KC_RBRC,  KC_GRV, KC_TILD,
-      _______, KC_LCBR, KC_LABK, KC_SCLN, KC_RABK, KC_RCBR, _______, _______, _______, _______, _______, TG(_ADJUST), KC_LCBR, KC_RCBR, KC_BSLS, _______,
-                                 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+       KC_DEL,  KC_LPRN, KC_SLSH,   KC_UP, KC_BSLS, KC_RPRN,                                     _______,  KC_INS, KC_LPRN, KC_RPRN, KC_PIPE,  KC_DEL,
+      KC_CAPS,  KC_LBRC, KC_LEFT, KC_DOWN,KC_RIGHT, KC_RBRC,                                     _______, _______, KC_LBRC, KC_RBRC,  KC_GRV, KC_TILD,
+       _______, KC_LCBR, KC_LABK, KC_SCLN, KC_RABK, KC_RCBR, _______, _______, _______, _______, _______, TG(_ADJUST), KC_LCBR, KC_RCBR, KC_BSLS, _______,
+                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
     [_ADJUST] = LAYOUT(
       _______,   KC_F7,   KC_F8,   KC_F9,  KC_F12,  KC_PGUP,                                     _______,    KC_7,    KC_8,    KC_9, _______, _______,
       KC_HOME,   KC_F4,   KC_F5,   KC_F6,  KC_F11,  KC_END,                                      KC_PPLS,    KC_4,    KC_5,    KC_6,    KC_0, KC_MINS,
       _______,   KC_F1,   KC_F2,   KC_F3,  KC_F10,  KC_PGDN, _______, _______, _______, _______,  KC_EQL,    KC_1,    KC_2,    KC_3, KC_UNDS, _______,
-                                 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+                                 _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______
     ),
 };
 
@@ -130,24 +130,24 @@ void encoder_update_user(uint8_t index, bool counter_clockwise) {
   if (index == 0) {
     switch(get_highest_layer(layer_state)){
       case _LOWER:
-        if (counter_clockwise){
-          tap_code(KC_COMM);
-        } else {
-          tap_code(KC_DOT);
-        }
-        break;
-      case _RAISE:
         if (counter_clockwise) {
             tap_code(KC_LEFT);
         } else {
             tap_code(KC_RIGHT);
         }
         break;
+      case _RAISE:
+        if (counter_clockwise){
+          tap_code(KC_COMM);
+        } else {
+          tap_code(KC_DOT);
+        }
+        break;
       case _ADJUST:
         if (counter_clockwise){
           tap_code(KC_MINS);
         } else {
-          tap_code(KC_PPLS);
+          tap_code(KC_EQL);
         }
         break;
       case _QWERTY:
@@ -163,17 +163,17 @@ void encoder_update_user(uint8_t index, bool counter_clockwise) {
   else if (index == 1) {
     switch(get_highest_layer(layer_state)){
       case _LOWER:
-        if (counter_clockwise){
-            tap_code(KC_UP);
-        } else {
-            tap_code(KC_DOWN);
-        }
-        break;
-      case _RAISE:
         if (counter_clockwise) {
             tap_code(KC_PGUP);
         } else {
             tap_code(KC_PGDN);
+        }
+        break;
+      case _RAISE:
+        if (counter_clockwise){
+            tap_code(KC_UP);
+        } else {
+            tap_code(KC_DOWN);
         }
         break;
       case _ADJUST:
