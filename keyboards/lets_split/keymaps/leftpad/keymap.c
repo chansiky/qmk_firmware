2f27eb35b36fb246f62d3f9df863a645b14467bd
Chan Youn <chansiky@gmail.com>
2021-05-20 08:41:43 -0500

    feat: lets_split:leftpad update

--- keyboards/lets_split/keymaps/leftpad/keymap.c
+++ keyboards/lets_split/keymaps/leftpad/keymap.c
@@ -45,26 +45,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_LOWER] = LAYOUT_ortho_4x12( \
-   KC_GRV, KC_COMM,    KC_7,    KC_8,    KC_9,  KC_DOT,   KC_HOME, KC_PGDN, KC_PGUP,  KC_END, _______, _______,\
-  KC_MINS,    KC_0,    KC_4,    KC_5,    KC_6, KC_PPLS,   KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, _______, _______,\
-  _______, KC_UNDS,    KC_1,    KC_2,    KC_3,  KC_EQL,   KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, _______,\
-  _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______ \
+ KC_GRV, KC_COMM,    KC_7,    KC_8,    KC_9,  KC_DOT,  KC_HOME, KC_PGDN, KC_PGUP,  KC_END, _______, _______,\
+KC_MINS,    KC_0,    KC_4,    KC_5,    KC_6, KC_PPLS,  KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, _______, KC_MINS,\
+_______, KC_UNDS,    KC_1,    KC_2,    KC_3,  KC_EQL,  KC_MUTE, KC_VOLD, KC_VOLU, _______, KC_UNDS, _______,\
+_______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______ \
 ),
 
 [_RAISE] = LAYOUT_ortho_4x12( \
-  KC_DEL, KC_LPRN, KC_SLSH,   KC_UP, KC_BSLS, KC_RPRN,   _______,  KC_INS, KC_LPRN, KC_RPRN, KC_PIPE,  KC_DEL,\
- KC_CAPS, KC_LBRC, KC_LEFT, KC_DOWN,KC_RIGHT, KC_RBRC,   _______, _______, KC_LBRC, KC_RBRC, KC_GRV,  KC_TILD,\
- _______, KC_LCBR, KC_LABK, KC_SCLN, KC_RABK, KC_RCBR,   _______,TG(_ADJUST),KC_LCBR, KC_RCBR, KC_BSLS, _______,\
- _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______ \
+  KC_DEL, KC_LPRN, KC_SLSH,   KC_UP, KC_BSLS, KC_RPRN,    _______, KC_INS, KC_LPRN, KC_RPRN,  KC_PIPE,  KC_DEL,\
+  KC_CAPS, KC_LBRC, KC_LEFT, KC_DOWN,KC_RIGHT, KC_RBRC,    _______, KC_LCBR, KC_LBRC, KC_RBRC,  KC_RCBR, KC_GRV,\
+  KC_ENT,KC_LCBR,KC_MS_BTN1,KC_MS_BTN3,KC_MS_BTN2,KC_RCBR, _______, _______, KC_LABK, KC_RABK, KC_BSLS, _______,\
+  _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______ \
 ),
 
 [_ADJUST] =  LAYOUT_ortho_4x12( \
-  _______,   KC_F7,   KC_F8,   KC_F9,  KC_F12,  KC_PGUP,  _______,    KC_7,    KC_8,    KC_9, _______, _______,\
-  KC_HOME,   KC_F4,   KC_F5,   KC_F6,  KC_F11,  KC_END,   KC_PPLS,    KC_4,    KC_5,    KC_6,    KC_0, KC_MINS,\
-  _______,   KC_F1,   KC_F2,   KC_F3,  KC_F10,  KC_PGDN,   KC_EQL,    KC_1,    KC_2,    KC_3, KC_UNDS, _______,\
-  _______, _______, _______, _______, _______,  _______,  _______, _______, _______, _______, _______, _______ \
+KC_PSCR,   KC_F7,   KC_F8,   KC_F9,  KC_F12,  KC_PGUP, KC_PAST, KC_KP_7, KC_KP_8, KC_KP_9, _______, _______,\
+KC_HOME,   KC_F4,   KC_F5,   KC_F6,  KC_F11,  KC_END,  KC_PPLS, KC_KP_4, KC_KP_5, KC_KP_6, KC_KP_0, KC_PMNS,\
+_______,   KC_F1,   KC_F2,   KC_F3,  KC_F10,  KC_PGDN, KC_EQL, KC_KP_1,  KC_KP_2, KC_KP_3, KC_UNDS, _______,\
+_______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______ \
 )
-
 };
 
 uint32_t layer_state_set_user(uint32_t state) {
